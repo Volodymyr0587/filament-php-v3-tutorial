ed891e5560a6f9709fc39cb5c046ad6f388a9575
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-11-18 20:11:01 +0200

    Customize FilamentPHP: disable darkmode option

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->font('Popins')
             ->favicon('images/laravel-icon.png')
+            // ->darkMode(false)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
