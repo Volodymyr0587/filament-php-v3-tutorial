b02c9ee2ebee4ca757e3a47d58247668a319498b
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-11-19 20:38:42 +0200

    Migrations, Models, Relationship

---
+++ app/Models/Product.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'brand_id',
+        'name',
+        'slug',
+        'sku',
+        'image',
+        'description',
+        'quantity',
+        'price',
+        'is_visible',
+        'is_featured',
+        'type',
+        'published_at',
+    ];
+
+    /**
+     * Get the brand associated with the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class)->withTimestamps();
+    }
+}
