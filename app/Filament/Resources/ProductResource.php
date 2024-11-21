9836f7dd40e08d236efdfe69d7cee33c4cf97b73
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-11-21 15:58:47 +0200

    Resource modifiers for form fields

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -2,28 +2,30 @@
 
 namespace App\Filament\Resources;
 
-use App\Enums\ProductTypeEnum;
-use App\Filament\Resources\ProductResource\Pages;
-use App\Filament\Resources\ProductResource\RelationManagers;
-use App\Models\Product;
 use Filament\Forms;
+use Filament\Tables;
+use App\Models\Product;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use App\Enums\ProductTypeEnum;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Section;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\Group;
-use Filament\Forms\Components\MarkdownEditor;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Toggle;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
-use Filament\Tables;
-use Filament\Tables\Columns\IconColumn;
-use Filament\Tables\Columns\ImageColumn;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\MarkdownEditor;
+use App\Filament\Resources\ProductResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ProductResource\RelationManagers;
 
 class ProductResource extends Resource
 {
@@ -43,35 +45,77 @@ class ProductResource extends Resource
                     ->schema([
                         Section::make()
                             ->schema([
-                                TextInput::make('name'),
-                                TextInput::make('slug'),
+                                TextInput::make('name')
+                                    ->required()
+                                    ->live(onBlur: true)
+                                    ->unique()
+                                    ->afterStateUpdated(function (string $operation, $state, Forms\Set $set) {
+                                        if ($operation !== 'create') {
+                                            return;
+                                        }
+                                        $set('slug', Str::slug($state));
+                                    }),
+
+                                TextInput::make('slug')
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->required()
+                                    ->unique(Product::class, 'slug', ignoreRecord: true),
+
                                 MarkdownEditor::make('description')
                                     ->columnSpan('full')
                             ])->columns(2),
 
                         Section::make('Pricing & Inventory')
                             ->schema([
-                                TextInput::make('sku'),
-                                TextInput::make('price'),
-                                TextInput::make('quantity'),
+                                TextInput::make('sku')
+                                    ->label('SKU (Stock Keeping Unit)')
+                                    ->unique()
+                                    ->required(),
+
+                                TextInput::make('price')
+                                    ->numeric()
+                                    ->rules(['regex:/^\d{1,6}(\.\d{0,2})?$/'])
+                                    ->required(),
+
+                                TextInput::make('quantity')
+                                    // ->rules(['integer', 'min:0']),
+                                    ->numeric()
+                                    ->minValue(0)
+                                    ->maxValue(100)
+                                    ->required(),
+
                                 Select::make('type')
                                     ->options([
                                         'downloadable' => ProductTypeEnum::DOWNLOADABLE->value,
                                         'deliverable' => ProductTypeEnum::DELIVERABLE->value
-                                    ])
+                                    ])->required()
                             ])->columns(2),
                     ]),
                 Group::make()
                     ->schema([
                         Section::make('Status')
                             ->schema([
-                                Toggle::make('is_visible'),
-                                Toggle::make('is_featured'),
-                                DatePicker::make('published_at')
+                                Toggle::make('is_visible')
+                                    ->label('Visibility')
+                                    ->helperText('Enable or disable product visibility')
+                                    ->default(true),
+
+                                Toggle::make('is_featured')
+                                    ->label('Featured')
+                                    ->helperText('Enable or disable product featured status'),
+
+                                DateTimePicker::make('published_at')
+                                    ->label('Availability')
+                                    ->default(now()),
                             ]),
                         Section::make('Image')
                             ->schema([
                                 FileUpload::make('image')
+                                    ->directory('form-attachments')
+                                    ->preserveFilenames()
+                                    ->image()
+                                    ->imageEditor()
                             ])->collapsible(),
 
                         Section::make('Associations')
