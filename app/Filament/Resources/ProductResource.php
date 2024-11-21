67913f7cb3b59f0c6cfce33c71dc5acf79323f2f
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-11-21 16:09:28 +0200

    Resource modifiers for table

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -133,14 +133,35 @@ class ProductResource extends Resource
         return $table
             ->columns([
                 ImageColumn::make('image'),
-                TextColumn::make('name'),
-                TextColumn::make('brand.name'),
-                IconColumn::make('is_visible')->boolean(),
-                TextColumn::make('price'),
-                TextColumn::make('quantity'),
-                TextColumn::make('published_at'), //->dateTime(),
-                TExtColumn::make('type')
 
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('brand.name')
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(),
+
+                IconColumn::make('is_visible')
+                    ->sortable()
+                    ->toggleable()
+                    ->label('Visibility')
+                    ->boolean(),
+
+                TextColumn::make('price')
+                    ->sortable()
+                    ->toggleable(),
+
+                TextColumn::make('quantity')
+                    ->sortable()
+                    ->toggleable(),
+
+                TextColumn::make('published_at')
+                    ->date()
+                    ->sortable(),
+
+                TextColumn::make('type')
 
             ])
             ->filters([
